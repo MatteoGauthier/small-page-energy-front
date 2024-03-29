0d17cbb3b49acdbcfa780213c87bfe587522f74b
ZeToMane <parente.pb@gmail.com>
2024-03-29 12:40:06 +0100

    Frontend

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			'white': '#FBFAFF',
+			'black': '#010102',
+			'gray100': '#F2F0FF',
+			'gray200': '#DFE6FC',
+			'blue100': '#91E0FF',
+			'blue200': '#6346F3',
+			'blue300': '#583ED7',
+			'blue400': '#492ECB',
+			'blue500': '#35209D',
+			'blue600': '#2D1B83',
+			'purple100': '#C8BBF3',
+			'purple200': '#9B52EF',
+			'purple300': '#8F13A5',
+			'pink100': '#F005AE',
+		},
+		extend: {},
+	},
+	plugins: [],
+};

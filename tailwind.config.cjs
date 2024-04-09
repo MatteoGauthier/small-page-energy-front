be98aa9dc08106bd45a60d74ff38cf6c34865a3d
evemarielouis <eve.tissot@mmibordeaux.com>
2024-04-09 13:26:51 +0300

    responsive front homepage

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+import { defaultSettings } from "astro/runtime/client/dev-toolbar/settings.js";
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -18,6 +20,25 @@ module.exports = {
 			'pink100': '#F005AE',
 		},
 		extend: {},
+		screens: {
+			'xs': {'min': '0px', 'max': '390px'},
+			// => @media (min-width: 390px) { ... }
+
+			'sm': '640px',
+			// => @media (min-width: 640px) { ... }
+	  
+			'md': '768px',
+			// => @media (min-width: 768px) { ... }
+	  
+			'lg': '1024px',
+			// => @media (min-width: 1024px) { ... }
+	  
+			'xl': '1280px',
+			// => @media (min-width: 1280px) { ... }
+	  
+			'2xl': '1536px',
+			// => @media (min-width: 1536px) { ... }
+		  },
 	},
 	plugins: [],
 };

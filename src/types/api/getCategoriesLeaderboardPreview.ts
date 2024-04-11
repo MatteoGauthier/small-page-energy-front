d4ccc6deb0277a77a2a276fc6d0e2c915fffba3a
Mattèo Gauthier <matteo.gauthier@gmail.com>
2024-04-11 14:31:09 +0200

    [TYPES] Fix type

--- src/types/api/getCategoriesLeaderboardPreview.ts
+++ src/types/api/getCategoriesLeaderboardPreview.ts
@@ -1,22 +1,32 @@
 // getCategoriesLeaderboardPreview
 
 export type CategoriesLeaderboardPreview = {
-  id: string
-  name: string
-  slug: string
-  ranks: Rank[]
+  id:    string;
+  name:  string;
+  slug:  string;
+  ranks: Rank[];
 }[]
 
-type Rank = {
-  website: Website
-  rank: number
-  change: null
-  siteWeight: number
+export type Rank = {
+  website:    Website;
+  rank:       number;
+  change:     null;
+  siteWeight: number;
 }
 
-type Website = {
-  id: string
-  name: string
-  url: string
+export type Website = {
+  id:         string;
+  name:       string;
+  url:        string;
+  categories: Category[];
+}
+
+export type Category = {
+  id:          string;
+  createdAt:   string;
+  updatedAt:   string;
+  name:        string;
+  slug:        string;
+  description: null;
 }
 

ba21f11bfbb002185e3673a33b41758d260cb32f
Mattèo Gauthier <matteo.gauthier@gmail.com>
2024-04-11 10:40:44 +0200

    [FEAT] Dynamic router by category (#3)

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,35 +1,36 @@
-import type { CategoriesLeaderboardPreview } from '../types/api/getCategoriesLeaderboardPreview'
+import type { Categories } from "../types/api/getCategories"
+import type { CategoriesLeaderboardPreview } from "../types/api/getCategoriesLeaderboardPreview"
 
-const BASE_URL = 'https://small-page-energy.osc-fr1.scalingo.io'
+const BASE_URL = "https://small-page-energy.osc-fr1.scalingo.io"
 
 const fetcher = async (endpoint: string) => {
-    const response = await fetch(`${BASE_URL}/api${endpoint}`, {
-        headers: {
-            Accept: 'application/json',
-            Authorization: `Bearer BQsZu102qg_Aq7RVnxmty`
-        }
-    })
-    if (!response.ok) {
-        throw new Error(response.statusText)
-    }
-    return response.json()
+  const response = await fetch(`${BASE_URL}/api${endpoint}`, {
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer BQsZu102qg_Aq7RVnxmty`,
+    },
+  })
+  if (!response.ok) {
+    throw new Error(response.statusText)
+  }
+  return response.json()
 }
 
 export const getWebsites = async () => {
-    return await fetcher(`/websites`)
+  return await fetcher(`/websites`)
 }
 
 export const getCategories = async () => {
-    return await fetcher(`/categories`)
+  return (await fetcher(`/categories`)) as Categories
 }
 export const getLeaderboard = async () => {
-    return await fetcher(`/leaderboard`)
+  return await fetcher(`/leaderboard`)
 }
 
 export const getLeaderboardByCategory = async (categoryId: string) => {
-    return await fetcher(`/leaderboard/${categoryId}`)
+  return await fetcher(`/leaderboard/${categoryId}`)
 }
 
 export const getCategoriesLeaderboardPreview = async () => {
-    return (await fetcher(`/categories-leaderboard-preview`)) as CategoriesLeaderboardPreview
+  return (await fetcher(`/categories-leaderboard-preview`)) as CategoriesLeaderboardPreview
 }
